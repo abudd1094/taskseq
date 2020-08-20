3a8b119c31dd3fa3c921f2e1833fef2d821081e0
Alec Budd <alec.budd@prodyna.com>
2020-08-20 14:29:34 +0200

    timer 0 realized

--- src/components/atoms/Timer.js
+++ src/components/atoms/Timer.js
@@ -1,20 +1,42 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import EStyleSheet from "react-native-extended-stylesheet";
 import { Typography } from "../../styles";
 import { lightGrey } from "../../styles/colors";
 import { Context } from "../../context/SequenceContext";
 
-const Timer = ({ duration, seqDuration, otherTasks, callback, small, style, active }) => {
+const Timer = ({ duration, seqDuration, callback, small, style, active }) => {
    const { state } = useContext(Context);
+   const localTimer = state.masterTimer - (seqDuration - duration);
+
+   const displayLocalTimer = () => {
+      if (state.timerOn && active && localTimer > 0) {
+         return localTimer;
+      } else if (state.timerOn && localTimer <= 0) {
+         return 0;
+      } else {
+         return duration;
+      }
+   };
+
+   useEffect(() => {
+      localTimer === 0 ? active = false : console.log('decr.');
+   });
 
    return (
-      <View style={styles.container}>
-         <Text style={[styles.timer, small ? styles.small : styles.large, style && style, active && { color: 'white' }]}>{
-            state.masterTimer - (seqDuration - duration)
-         }</Text>
-      </View>
-   )
+     <View style={styles.container}>
+       <Text
+         style={[
+           styles.timer,
+           small ? styles.small : styles.large,
+           style && style,
+           active && { color: "white" },
+         ]}
+       >
+         {displayLocalTimer()}
+       </Text>
+     </View>
+   );
 };
 
 const styles = EStyleSheet.create({
